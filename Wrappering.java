0bb66c6dd2bf3e4bcdc770b0a37bfe57d6a9d1c3
subhadipde09 <subhadipdeece22l65@gmail.com>
2025-03-10 11:21:56 +0530

    code added

--- Wrappering.java
+++ Wrappering.java
@@ -1,4 +1,25 @@
-package PACKAGE_NAME;
-
+import java.io.*;
 public class Wrappering {
+    public static void main(String []x)
+    {
+        // primitive data types.
+        byte b=1;
+        int i=10;
+        float f=12.5f;
+        double d=560.56;
+        char c='m';
+
+        //converting into wrapper object
+        Byte b1=Byte.valueOf(b);
+        Integer i1=Integer.valueOf(i);
+        Float f1=Float.valueOf(f);
+        Double d1=Double.valueOf(d);
+        Character c1=Character.valueOf(c);
+
+        System.out.println(b1);
+        System.out.println(i1);
+        System.out.println(f1);
+        System.out.println(d1);
+        System.out.println(c1);
+    }
 }
