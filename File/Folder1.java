64412c74f73512852ac7c7b59eea04e20ef0ea7e
subhadipde09 <subhadipdeece22l65@gmail.com>
2025-03-10 18:28:19 +0530

    updated code

---
+++ File/Folder1.java
@@ -0,0 +1,15 @@
+package File;
+public class Folder1 {
+    private int x ;
+    private int y;
+    public void setData(int p,int q)
+    {
+       x=p;
+       y=q;
+    }
+    public void display()
+    {
+        System.out.println(x);
+        System.out.println(y);
+    }
+}
---
+++ Folder2.java
@@ -0,0 +1,8 @@
+import File.Folder1;
+public class Folder2 {
+    public static void main(String []x){
+        Folder1 f1=new Folder1();
+        f1.setData(6,7);
+        f1.display();
+    }
+}
